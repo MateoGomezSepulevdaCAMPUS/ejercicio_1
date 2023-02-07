bb5abadcba6397cfc6e2bab4886e8bc03d1aaf32
MateoGomezSepulevdaCAMPUS <mateos20900@gmail.com>
2023-02-07 09:20:29 -0500

    ejercicio6

---
+++ ejercicio6.py
@@ -0,0 +1,11 @@
+'''En sus propias palabras, qué son las funciones
+preconstruidas y proporcione 2 ejemplos '''
+
+'''Una función es un bloque de código, que recibe cero o más argumentos y que una operación para devolvernos un valor y/o realiza 
+una tarea específica. Las funciones deben tener un nombre asignado para poder “llamarlas” durante la ejecución del programa y son 
+completamente reutilizables.
+Funciones predefinidas más reseñables 
+    list() Crea una lista a partir de un elemento.
+    tuple() Crea o convierte en una tupla.
+    type() Devuelve el tipo del elemento pasado como parámetro.
+    '''
