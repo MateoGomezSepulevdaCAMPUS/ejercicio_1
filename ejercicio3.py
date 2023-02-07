a9a9262b29cfc31c51a493587885bd3e65731f4b
MateoGomezSepulevdaCAMPUS <mateos20900@gmail.com>
2023-02-07 08:21:20 -0500

    ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,9 @@
+'''Si hay operadores de igual precedencia, en qué orden se
+ejecutan?
+A. De izquierda a derecha
+B. De derecha a izquierda 
+
+Cuando se producen varias operaciones en una expresión, cada parte se evalúa y resuelve en un orden predeterminado
+denominado prioridad de operadores.
+'''
+
