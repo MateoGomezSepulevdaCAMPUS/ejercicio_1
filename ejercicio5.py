5b1b7a48085e2d59b5ff66ee0cdd4c2ec2e38861
MateoGomezSepulevdaCAMPUS <mateos20900@gmail.com>
2023-02-07 09:20:11 -0500

    ejercicio5

---
+++ ejercicio5.py
@@ -0,0 +1,21 @@
+'''Enumere 5 tipos de datos en Python y suministre un valor de
+ejemplo de cada uno. '''
+
+'integer:Números enteros, por ejemplo: 1, 2, 3 … 100 -100'
+a=int
+a=4
+print("Muestre el numero entero",a)
+
+
+b=float
+b=4.5
+print("Muestre el numero flotante",b)
+
+Es_conductor = True
+print("Es conductor")
+
+no_tiene_manos = False
+print("No tiene manos")
+
+
+
