599a960a9cc962eb2fd95a4a074f37c3354888c2
MateoGomezSepulevdaCAMPUS <mateos20900@gmail.com>
2023-02-07 09:19:54 -0500

    ejericicio4

---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+'''¿Que son las expresiones regulares en Python?
+
+Las expresiones regulares, a menudo llamada también regex, son unas secuencias de caracteres que forma un patrón de búsqueda
+, las cuales son formalizadas por medio de una sintaxis específica. Los patrones se interpretan como un conjunto de instrucciones
+, que luego se ejecutan sobre un texto de entrada para producir un subconjunto o una versión modificada del texto original. 
+Las expresiones regulares pueden incluir patrones de coincidencia literal, de repetición, de composición, de ramificación, y otras
+sofisticadas reglas de reconocimiento de texto . Las expresiones regulares deberían formar parte del arsenal de cualquier buen 
+programador ya que un gran número de problemas de procesamiento de texto pueden ser fácilmente resueltos con ellas.
+'''
