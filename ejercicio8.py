0ff264204003417776d20db5f86b5762f0cfda5a
MateoGomezSepulevdaCAMPUS <mateos20900@gmail.com>
2023-02-13 10:49:20 -0500

    guardado

---
+++ ejercicio8.py
@@ -0,0 +1,12 @@
+'''Construya un algoritmo en Python, que permita ingresar la
+información de un empleado e imprima el nombre, los
+apellidos y la antigüedad. Los datos que se deben solicitar
+son los siguientes:
+*Nombre * Teléfono *Año de ingreso a la empresa
+*Apellidos *Edad.'''
+
+nombre = input("Por favor ingrese su nombre: ")
+apellidos = input("Por favor ingrese su apellidos: ")
+telefono = int(input("Por favor ingrese su numero de celular: "))
+edad = int(input("Por favor ingrese su edad: "))
+año_ingreso = int(input("Por favor ingrese el año que ingreso a la empresa: "))
